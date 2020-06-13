ac4b03cafd24281b2f2f7a64203fb91933cd30e4
Michal Czarnecki <mczarnecki11@gmail.com>
2020-06-14 00:16:07 +0200

    Added LKM over CHRDEV

--- lkm/jajko.c
+++ lkm/jajko.c
@@ -1,36 +1,21 @@
-/**
- * @file   ebbchar.c
- * @brief   An introductory character driver. This module maps to /dev/ebbchar and
- * comes with a helper C program that can be run in Linux user space to communicate with
- * this the LKM.
- */
- 
 #include <linux/init.h>           // Macros used to mark up functions e.g. __init __exit
 #include <linux/module.h>         // Core header for loading LKMs into the kernel
 #include <linux/device.h>         // Header to support the kernel Driver Model
 #include <linux/kernel.h>         // Contains types, macros, functions for the kernel
 #include <linux/fs.h>             // Header for the Linux file system support
 #include <linux/uaccess.h>          // Required for the copy to user function
-#define  DEVICE_NAME "ebbchar"    ///< The device will appear at /dev/ebbchar using this value
-#define  CLASS_NAME  "ebb"        ///< The device class -- this is a character device driver
+
+#include "jajko.h"
  
 MODULE_LICENSE("GPL");            ///< The license type -- this affects available functionality
-MODULE_AUTHOR("Author Name");    ///< The author -- visible when you use modinfo
-MODULE_DESCRIPTION("A simple Linux char driver for the BBB");  ///< The description -- see modinfo
+MODULE_AUTHOR("MC");    ///< The author -- visible when you use modinfo
+MODULE_DESCRIPTION("Linux Kernel Module over chrdev.");  ///< The description -- see modinfo
 MODULE_VERSION("0.1");            ///< A version number to inform users
  
 static int    majorNumber;                  ///< Stores the device number -- determined automatically
-static char   message[256] = {0};           ///< Memory for the string that is passed from userspace
-static short  size_of_message;              ///< Used to remember the size of the string stored
-static int    numberOpens = 0;              ///< Counts the number of times the device is opened
-static struct class*  ebbcharClass  = NULL; ///< The device-driver class struct pointer
-static struct device* ebbcharDevice = NULL; ///< The device-driver device struct pointer
- 
-// The prototype functions for the character driver -- must come before the struct definition
-static int     dev_open(struct inode *, struct file *);
-static int     dev_release(struct inode *, struct file *);
-static ssize_t dev_read(struct file *, char *, size_t, loff_t *);
-static ssize_t dev_write(struct file *, const char *, size_t, loff_t *);
+static char   message[BUFFER_SIZE] = {0};           ///< Memory for the string that is passed from userspace
+static struct class*  jajkoClass  = NULL; ///< The device-driver class struct pointer
+static struct device* jajkoDevice = NULL; ///< The device-driver device struct pointer
 
 static struct file_operations fops =
 {
@@ -38,79 +23,58 @@ static struct file_operations fops =
    .write = dev_write,
 };
 
-static int __init ebbchar_init(void){
-   printk(KERN_INFO "EBBChar: Initializing the EBBChar LKM\n");
+static int __init jajko_init(void){
+   printk(KERN_INFO "jajko: Initializing the jajko LKM\n");
  
    // Try to dynamically allocate a major number for the device -- more difficult but worth it
    majorNumber = register_chrdev(0, DEVICE_NAME, &fops);
    if (majorNumber<0){
-      printk(KERN_ALERT "EBBChar failed to register a major number\n");
+      printk(KERN_ALERT "jajko failed to register a major number\n");
       return majorNumber;
    }
-   printk(KERN_INFO "EBBChar: registered correctly with major number %d\n", majorNumber);
+   printk(KERN_INFO "jajko: registered correctly with major number %d\n", majorNumber);
  
    // Register the device class
-   ebbcharClass = class_create(THIS_MODULE, CLASS_NAME);
-   if (IS_ERR(ebbcharClass)){                // Check for error and clean up if there is
+   jajkoClass = class_create(THIS_MODULE, CLASS_NAME);
+   if (IS_ERR(jajkoClass)){                // Check for error and clean up if there is
       unregister_chrdev(majorNumber, DEVICE_NAME);
       printk(KERN_ALERT "Failed to register device class\n");
-      return PTR_ERR(ebbcharClass);          // Correct way to return an error on a pointer
+      return PTR_ERR(jajkoClass);          // Correct way to return an error on a pointer
    }
-   printk(KERN_INFO "EBBChar: device class registered correctly\n");
+   printk(KERN_INFO "jajko: device class registered correctly\n");
  
    // Register the device driver
-   ebbcharDevice = device_create(ebbcharClass, NULL, MKDEV(majorNumber, 0), NULL, DEVICE_NAME);
-   if (IS_ERR(ebbcharDevice)){               // Clean up if there is an error
-      class_destroy(ebbcharClass);           // Repeated code but the alternative is goto statements
+   jajkoDevice = device_create(jajkoClass, NULL, MKDEV(majorNumber, 0), NULL, DEVICE_NAME);
+   if (IS_ERR(jajkoDevice)){               // Clean up if there is an error
+      class_destroy(jajkoClass);           // Repeated code but the alternative is goto statements
       unregister_chrdev(majorNumber, DEVICE_NAME);
       printk(KERN_ALERT "Failed to create the device\n");
-      return PTR_ERR(ebbcharDevice);
+      return PTR_ERR(jajkoDevice);
    }
-   printk(KERN_INFO "EBBChar: device class created correctly\n"); // Made it! device was initialized
+   printk(KERN_INFO "jajko: device class created correctly\n"); // Made it! device was initialized
    return 0;
 }
 
-static void __exit ebbchar_exit(void){
-   device_destroy(ebbcharClass, MKDEV(majorNumber, 0));     // remove the device
-   class_unregister(ebbcharClass);                          // unregister the device class
-   class_destroy(ebbcharClass);                             // remove the device class
+static void __exit jajko_exit(void){
+   device_destroy(jajkoClass, MKDEV(majorNumber, 0));     // remove the device
+   class_unregister(jajkoClass);                          // unregister the device class
+   class_destroy(jajkoClass);                             // remove the device class
    unregister_chrdev(majorNumber, DEVICE_NAME);             // unregister the major number
-   printk(KERN_INFO "EBBChar: Goodbye from the LKM!\n");
-}
-
-static int dev_open(struct inode *inodep, struct file *filep){
-   numberOpens++;
-   printk(KERN_INFO "EBBChar: Device has been opened %d time(s)\n", numberOpens);
-   return 0;
+   printk(KERN_INFO "jajko: Goodbye from the LKM!\n");
 }
  
 static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *offset){
-   int error_count = 0;
    // copy_to_user has the format ( * to, *from, size) and returns 0 on success
-   error_count = copy_to_user(buffer, message, size_of_message);
- 
-   if (error_count==0){            // if true then have success
-      printk(KERN_INFO "EBBChar: Sent %d characters to the user\n", size_of_message);
-      return (size_of_message=0);  // clear the position to the start and return 0
-   }
-   else {
-      printk(KERN_INFO "EBBChar: Failed to send %d characters to the user\n", error_count);
-      return -EFAULT;              // Failed -- return a bad address message (i.e. -14)
-   }
-}
-
-static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, loff_t *offset){
-   sprintf(message, "%s(%zu letters)", buffer, len);   // appending received string with its length
-   size_of_message = strlen(message);                 // store the length of the stored message
-   printk(KERN_INFO "EBBChar: Received %zu characters from the user\n", len);
+   copy_to_user(buffer, message, BUFFER_SIZE);
    return len;
 }
 
-static int dev_release(struct inode *inodep, struct file *filep){
-   printk(KERN_INFO "EBBChar: Device successfully closed\n");
-   return 0;
+static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, loff_t *offset){
+    strcpy(message, buffer);
+    return len;
 }
 
-module_init(ebbchar_init);
-module_exit(ebbchar_exit);
+
+module_init(jajko_init);
+module_exit(jajko_exit);
 
--- lkm/jajko.h
+++ lkm/jajko.h
@@ -1,6 +1,8 @@
 #ifndef _JAJKO_H_
 #define _JAJKO_H_
-#include <sys/types.h>
+
+#define DEVICE_NAME "jajko"
+#define CLASS_NAME "jajko"
 #define BUFFER_SIZE 32
 
 static ssize_t dev_read(struct file *, char *, size_t, loff_t *);
